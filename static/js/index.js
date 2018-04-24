13f5586b2f26fc4aacf804603fd7758de1bf6c51
Jared Johnson <jared@nuts.com>
2018-04-24 09:53:18 -0400

    Open resume menu item in newtab.

--- static/js/index.js
+++ static/js/index.js
@@ -3,3 +3,6 @@ const isProduction = location.hostname.toLowerCase().endsWith('jaredcodes.com');
 if (isProduction && location.protocol !== 'https:') {
   location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
 }
+
+// Open resume menu item in newtab
+document.querySelectorAll('a[href="/assets/jared-johnson-resume.pdf"]').forEach(el => el.target="newtab");
