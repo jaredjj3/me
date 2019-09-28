7067dfb106fc42ca5137e15fe005ed321148ad98
Jared Johnson <jared@jaredjohnson.dev>
2019-09-28 11:56:58 -0400

    Add basic files.

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+// Redirect to HTTPS if not in development
+const isProduction = location.hostname.toLowerCase().endsWith('jaredjohnson.dev');
+if (isProduction && location.protocol !== 'https:') {
+  location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
+
+// Open resume menu item in newtab
+document.querySelectorAll('a[href="/assets/jared-johnson-resume.pdf"]').forEach(el => el.target="newtab");
